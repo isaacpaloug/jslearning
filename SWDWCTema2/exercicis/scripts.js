b56d0dd9a40e64d2f19710bae26fd5e55f2c8d85
isaacpaloug <isaacpalou@paucasesnovescifp.cat>
2022-10-07 20:46:08 +0200

    feat :sparkles: add exercise 1

--- SWDWCTema2/exercicis/scripts.js
+++ SWDWCTema2/exercicis/scripts.js
@@ -1,3 +1,10 @@
 function exercici1(){
     var radi = prompt("Donem el radi de la circumferència");
-}
+    if (!isNaN(radi)) {
+        var longitud = 2 * Math.PI * radi;
+        console.log("El teu radi és: " + radi + " i la longitud és: " + longitud);
+    }else{
+        prompt("El que has introduit no es un numero")
+    }
+    
+}go
