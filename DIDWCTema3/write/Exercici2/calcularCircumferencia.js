c19df3cc9e40736f8894df04aae95ee2b74be82a
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-10-19 17:31:55 +0200

    add :sparkles: Acabar exercici T3

--- DIDWCTema3/write/Exercici2/calcularCircumferencia.js
+++ DIDWCTema3/write/Exercici2/calcularCircumferencia.js
@@ -1,12 +1,11 @@
-'use strict'
+"use strict";
 function calcular1() {
-    let radi = document.getElementById("radi").value;
-    let longitud = 2 * Math.PI * radi;
-    document.write("Longitud: " + longitud)
+  let radi = document.getElementById("radi").value;
+  let longitud = 2 * Math.PI * radi;
+  document.write("Longitud: " + longitud);
 }
 function calcular2() {
-    let radi = document.getElementById("radi").value;
-    let longitud = 2 * Math.PI * radi;
-    var paragraf = document.getElementById("longitud");
-    paragraf.innerHTML = longitud;
+  let radi = document.getElementById("radi").value;
+  let longitud = 2 * Math.PI * radi;
+  document.getElementById("longitud").innerHTML = longitud;
 }
