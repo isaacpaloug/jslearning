f6a5b0338d7131d04b2e995520655b1b8ff3998a
isaacpaloug <isaacpalou@paucasesnovescifp.cat>
2022-10-30 17:16:57 +0100

    Objectes acabats

--- SWDWCTema4/usuari/js/alumnes.js
+++ SWDWCTema4/usuari/js/alumnes.js
@@ -1,13 +1,27 @@
-class Alumne{
-    constructor(nom, llinatges, curs,moduls){
-        this.nom = nom;
-        this.llinatges = llinatges;
-        this.curs = curs;
-        this.moduls = moduls;
-    };
-    afegirModul(newModul){
+class Alumne {
+  constructor(nom, llinatges, curs, moduls) {
+    this.nom = nom;
+    this.llinatges = llinatges;
+    this.curs = curs;
+    this.moduls = this.llevarRepetits(moduls);
+  }
+  // afegirModul(newModul){
+  // }
+  llevarRepetits(moduls) {
+    let modulsNoRep = new Set();
+    for (let iterator of moduls) {
+      modulsNoRep.add(iterator);
     }
-    get NomComplet(){
-        return this.llinatges + this.nom;
-    }
+    return modulsNoRep;
+  }
+  get NomComplet() {
+    return this.llinatges + this.nom;
+  }
+
+  afegirModul(modul){
+    this.moduls.add(modul);
+  }
+  borrarModul(modul) {
+    this.moduls.delete(modul);
+  }
 }
--- SWDWCTema4/usuari/js/moduls.js
+++ SWDWCTema4/usuari/js/moduls.js
@@ -1,7 +1,18 @@
-class Modul{
-    constructor(nom, codi, curs){
-        this.nom = nom;
-        this.codi = codi;
-        this.curs = curs;
-    }
+class Modul {
+  constructor(nom, codi, curs) {
+    this.nom = nom;
+    this.codi = codi;
+    this.curs = curs;
+    this.toString = function () {
+      let missatge =
+        "[ Nom = " +
+        this.nom +
+        " Codi = " +
+        this.codi +
+        " Curs = " +
+        this.curs +
+        " ]";
+      return missatge;
+    };
+  }
 }
