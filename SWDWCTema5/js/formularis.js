fbb155efccd0c75a501f2d76aa8206deb907cc7b
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-11-03 19:32:06 +0100

    e

--- SWDWCTema5/js/formularis.js
+++ SWDWCTema5/js/formularis.js
@@ -1,20 +1,24 @@
 function marcar() {
-  alert("hola");
+  let chk = document.myform.check_list;
+  if (document.getElementById("checkbox1").checked) {
+    for (i = 0; i < chk.length; i++) chk[i].checked = false;
+  }else{
+    for (i = 0; i < chk.length; i++) chk[i].checked = true;
+  }
 }
 function deshabilitar() {
-    let nom = document.getElementById("nom");
-    if (nom.disabled) {
-        nom.disabled = false;
-    }else{
-        nom.disabled = true;
-    }
+  let nom = document.getElementById("nom");
+  if (nom.disabled) {
+    nom.disabled = false;
+  } else {
+    nom.disabled = true;
+  }
 }
 function convertLectura() {
-    let nom = document.getElementById("password");
-    if(nom.readOnly){
-        nom.readOnly = false;
-    }else{
-        nom.readOnly = true;
-    }
-
+  let nom = document.getElementById("password");
+  if (nom.readOnly) {
+    nom.readOnly = false;
+  } else {
+    nom.readOnly = true;
+  }
 }
