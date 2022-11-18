faff9ca417c79f0b0ea099e4f94470f96190e35c
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-11-18 17:50:26 +0100

    formularis no acabat

---
+++ SWDWCTema5/js/validarFormulari.js
@@ -0,0 +1,10 @@
+"use strict"
+import {
+    validarObligatori,
+    validarData,
+    validarTelefon,
+    validarTelefonMobil,
+    validarNIF,
+    validarSencer,
+    validarDecimal
+} from './validacions.js';
