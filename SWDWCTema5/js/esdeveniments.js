9f1e3aeb3e5b8aa5e59a5336ed43e6fce69ad318
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2022-12-13 19:00:25 +0100

    e

--- SWDWCTema5/js/esdeveniments.js
+++ SWDWCTema5/js/esdeveniments.js
@@ -13,70 +13,70 @@ paragraf.addEventListener("mousedown", function () {
     afegeix("paragraf: mousedown ");
 });
 
-paragraf.addEventListener("mouseenter", function(){
+paragraf.addEventListener("mouseenter", function () {
     afegeix("paragraf: mouseenter ")
 });
 
-paragraf.addEventListener("mouseout", function(){
+paragraf.addEventListener("mouseout", function () {
     afegeix("paragraf: mouseout ")
 });
 
-paragraf.addEventListener("mouseleave", function(){
+paragraf.addEventListener("mouseleave", function () {
     afegeix("paragraf: mouseleave ")
 });
 
-paragraf.addEventListener("mousemove", function(){
+paragraf.addEventListener("mousemove", function () {
     afegeix("paragraf: mousemove ")
 });
-paragraf.addEventListener("dblclick", function(){
+paragraf.addEventListener("dblclick", function () {
     afegeix("paragraf: dblclick ")
 });
 // ! ESDEVENIMENTS DE FORM
 
-    // ? INPUT TEXT
+// ? INPUT TEXT
 let text = document.getElementById("input_text");
 
-text.addEventListener("focus", function(){
+text.addEventListener("focus", function () {
     afegeix("input: onfocus")
 });
 
-text.addEventListener("focus", function(){
+text.addEventListener("focus", function () {
     afegeix("input: onfocus")
 });
 
-text.addEventListener("blur", function(){
+text.addEventListener("blur", function () {
     afegeix("input: onblur")
 });
-text.addEventListener("focusout", function(){
+text.addEventListener("focusout", function () {
     afegeix("input: onfocusout")
 });
-text.addEventListener("change", function(){
+text.addEventListener("change", function () {
     afegeix("input: onchange")
 });
-text.addEventListener("input", function(){
+text.addEventListener("input", function () {
     afegeix("input: oninput")
 });
-text.addEventListener("select", function(){
+text.addEventListener("select", function () {
     afegeix("input: onselect")
 })
-    // ? SUBMIT
+// ? SUBMIT
 
 let formulari = document.getElementById("form");
 
-formulari.addEventListener("submit", function(event){
+formulari.addEventListener("submit", function (event) {
     event.preventDefault();
 })
-formulari.addEventListener("submit", function(){
+formulari.addEventListener("submit", function () {
     afegeix("submit: onsubmit")
 })
-    // ? RESET
+// ? RESET
 
-formulari.addEventListener("reset", function(){
+formulari.addEventListener("reset", function () {
     afegeix("reset: onreset");
 });
-window.onload = function(){
+window.onload = function () {
     afegeix("window: onload")
 }
-window.onbeforeunload = function(){
+window.onbeforeunload = function () {
     afegeix("window: onbeforeunload")
 }
--- SWWCTema6/js/desitjos3.js
+++ SWWCTema6/js/desitjos3.js
@@ -1,18 +1,16 @@
 'use strict';
-
-function elimina(e) {
+// ! Funció per esborrar l'element de la llista
+function esborrar(e) {
     const objecte = e.target;
     const liObjecte = objecte.parentNode;
     const ulObjecte = liObjecte.parentNode;
     ulObjecte.removeChild(liObjecte);
-
 }
 
 function canviaLlista(e) {
     const objecte = e.target;
     const liObjecte = objecte.parentNode;
     const ulObjecte = liObjecte.parentNode;
-    //ulObjecte.removeChild(liObjecte);
     let novaUl;
     if (ulObjecte === document.getElementById("llistaDemanats")) {
         novaUl = document.getElementById("llistaComplits");
@@ -22,37 +20,30 @@ function canviaLlista(e) {
     novaUl.appendChild(liObjecte);
 }
 
-
+// ! Funció per afegir un element a la llista.
 function afegir() {
+    // * @param nou -> Sirà el valor del nou element de la llista
+    // * @param nouLi -> Crea un element del tipus <li></li>
+    // * @param text -> Crea el text sencer que dura dins l'element li
     const nou = document.getElementById("nou").value;
     const nouLi = document.createElement("li");
     const text = document.createTextNode(nou + " ");
+    // * Ficar el text dins l'element li
     nouLi.appendChild(text);
 
-    //Crear boto Esborra
+    // ? Crear boto Esborra
     const bEsborra = document.createElement("button");
     bEsborra.setAttribute("type", "button");
     bEsborra.appendChild(document.createTextNode("Borrar"));
     nouLi.appendChild(bEsborra);
-    bEsborra.addEventListener("click",elimina)
+    bEsborra.addEventListener("click",esborrar)
 
-    //Crear boto Canvia
+    // ?Crear boto Canvia
     const bCanvia = document.createElement("button");
     bCanvia.setAttribute("type", "button");
     bCanvia.appendChild(document.createTextNode("Canviar"));
     nouLi.appendChild(bCanvia);
-    bCanvia.addEventListener("click", function (esdeveniment) {
-        const objecte = esdeveniment.target;
-        const liObjecte = objecte.parentNode;
-        const ulObjecte = nouLi.parentNode;
-        let novaUl;
-        if (ulObjecte === document.getElementById("llistaDemanats")) {
-            novaUl = document.getElementById("llistaComplits");
-        } else {
-            novaUl = document.getElementById("llistaDemanats");
-        }
-        novaUl.appendChild(liObjecte);
-    });
+    bCanvia.addEventListener("click", canviaLlista);
 
     document.getElementById("llistaDemanats").appendChild(nouLi);
 }
